aee754de77a4eec3c152068495242aa9ad8d5170
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-16 16:36:56 +1000

    css&test&folder

--- src/Multichoice/MultiChoice.js
+++ src/Multichoice/MultiChoice.js
@@ -7,7 +7,7 @@ class MultiChoice extends Component {
         {this.props.answers.map((answer, i) => <button key={i} onClick={() => this.props.updateSelected(answer)}>{answer}</button>)}
         <br />
         <p>You have selected: {this.props.selectedAnswer}</p>
-        // <button onClick={() => {console.log(this); this.props.handleSubmit()}}>Submit</button>
+        {/*<button onClick={() => {console.log(this); this.props.handleSubmit()}}>Submit</button>*/} 
         <button onClick={this.props.handleSubmit}> Submit </button>
       </div>
     )
