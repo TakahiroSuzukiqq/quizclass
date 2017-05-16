09a7c13bac2cc236631469ba114b23aff6173646
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-16 14:25:00 +1000

    submit&resultn

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import Question from './Question';
 import ProgressBar from './ProgressBar';
+import MultiChoice from './MultiChoice';
+import Results from './Results';
 
 class App extends Component {
   constructor(props) {
      super(props);
      this.state = {
-       progress: 0
+       progress: 0,
+       selected: 'Not yet!',
+       score: 0
      };
+     this.updateSelected = this.updateSelected.bind(this);
+     this.submitAnswer = this.submitAnswer.bind(this);
      this.quiz_data = [
        {
          question: 'What is the meaning of life?',
@@ -22,12 +28,48 @@ class App extends Component {
        }
      ]
   }
+
+submitAnswer(){
+  console.log("submitAnswer()");
+  if (this.state.selected === this.quiz_data[this.state.progress].correct_answer) {
+      this.setState({
+        score: this.state.score + 1,
+        progress: this.state.progress + 1,
+        selected: 'Not yet!'
+      })
+  } else {
+    this.setState({
+      progress: this.state.progress + 1,
+      selected: 'Not yet!'
+    })
+  }
+}
+
+  updateSelected(answer) {
+    this.setState({
+      selected: answer
+    })
+  }
+
   render() {
+    const in_quiz = (
+      <div>
+        <Question current_question={this.quiz_data[this.state.progress].question} />
+        <ProgressBar current_step={this.state.progress+ 1} question_length={this.quiz_data.length} />
+        <MultiChoice
+        answers={this.quiz_data[this.state.progress].possible_answers}
+        updateSelected={this.updateSelected}
+        handleSubmit={this.submitAnswer}
+        selectedAnswer={this.state.selected} />
+      </div>
+    );
+    const results_component = (
+      <Results score={this.state.score} end_message="Congratulation, you have finished!" />
+    );
     return (
       <div>
-          <h2>Quiz App</h2>
-          <Question current_question={this.quiz_data[this.state.progress].question} />
-          <ProgressBar current_step={this.state.progress+ 1} question_length={this.quiz_data.length} />
+        <h2>Quiz App</h2>
+          {this.state.progress >= this.quiz_data.length ? results_component : in_quiz}
       </div>
     );
   }
