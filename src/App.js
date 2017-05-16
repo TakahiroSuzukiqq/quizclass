66c68b3fbcde55968f59fb9da755cf56a2947e86
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-16 15:41:30 +1000

    restart buton

--- src/App.js
+++ src/App.js
@@ -7,43 +7,44 @@ import Results from './Results';
 
 class App extends Component {
   constructor(props) {
-     super(props);
-     this.state = {
-       progress: 0,
-       selected: 'Not yet!',
-       score: 0
-     };
-     this.updateSelected = this.updateSelected.bind(this);
-     this.submitAnswer = this.submitAnswer.bind(this);
-     this.quiz_data = [
-       {
-         question: 'What is the meaning of life?',
-         correct_answer: '42',
-         possible_answers: ['Love', 'Money', 'Javascript', '42']
-       },
-       {
-         question: 'Who is the best coding youtuber?',
-         correct_answer: 'FunFunFunction',
-         possible_answers: ['Coding Train', 'Younglamb', 'Wes Bos', 'FunFunFunction']
-       }
-     ]
+    super(props);
+    this.state = {
+      progress: 0,
+      selected: 'Not yet!',
+      score: 0
+    };
+    this.updateSelected = this.updateSelected.bind(this);
+    this.submitAnswer = this.submitAnswer.bind(this);
+    this.handleRestart = this.handleRestart.bind(this);
+    this.quiz_data = [
+      {
+        question: 'What is the meaning of life?',
+        correct_answer: '42',
+        possible_answers: ['Love', 'Money', 'JavaScript', '42']
+      },
+      {
+        question: 'Who is the best coding YouTuber?',
+        correct_answer: 'FunFunFunction',
+        possible_answers: ['Coding Train', 'FunFunFunction', 'Young Lamb', 'Wes Bos']
+      }
+    ]
   }
 
-submitAnswer(){
-  console.log("submitAnswer()");
-  if (this.state.selected === this.quiz_data[this.state.progress].correct_answer) {
+
+  submitAnswer() {
+    if (this.state.selected === this.quiz_data[this.state.progress].correct_answer) {
       this.setState({
         score: this.state.score + 1,
         progress: this.state.progress + 1,
         selected: 'Not yet!'
       })
-  } else {
-    this.setState({
-      progress: this.state.progress + 1,
-      selected: 'Not yet!'
-    })
+    } else {
+      this.setState({
+        progress: this.state.progress + 1,
+        selected: 'Not yet!'
+      })
+    }
   }
-}
 
   updateSelected(answer) {
     this.setState({
@@ -51,25 +52,33 @@ submitAnswer(){
     })
   }
 
+
+  handleRestart() {
+    this.setState({
+      progress: 0,
+      score: 0
+    })
+  }
+
   render() {
-    const in_quiz = (
-      <div>
-        <Question current_question={this.quiz_data[this.state.progress].question} />
-        <ProgressBar current_step={this.state.progress+ 1} question_length={this.quiz_data.length} />
-        <MultiChoice
-        answers={this.quiz_data[this.state.progress].possible_answers}
-        updateSelected={this.updateSelected}
-        handleSubmit={this.submitAnswer}
-        selectedAnswer={this.state.selected} />
-      </div>
-    );
-    const results_component = (
-      <Results score={this.state.score} end_message="Congratulation, you have finished!" />
-    );
     return (
       <div>
         <h2>Quiz App</h2>
-          {this.state.progress >= this.quiz_data.length ? results_component : in_quiz}
+        {this.state.progress < this.quiz_data.length ? (
+          <div>
+            <Question current_question={this.quiz_data[this.state.progress].question} />
+            <ProgressBar current_step={this.state.progress + 1} question_length={this.quiz_data.length} />
+            <MultiChoice
+              answers={this.quiz_data[this.state.progress].possible_answers}
+              updateSelected={this.updateSelected}
+              handleSubmit={this.submitAnswer}
+              selectedAnswer={this.state.selected} />
+          </div>
+        )
+        : (
+        <Results score={this.state.score} end_message="Congratulations, you have finished!"
+           handleRestart={this.handleRestart} />
+         )}
       </div>
     );
   }
