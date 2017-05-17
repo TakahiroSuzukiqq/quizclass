77ee39fe05b4b52de7605ba87da7a886f348e586
鈴木貴大 <TakahiroSuzuki@suzukikidai-no-MacBook-Air.local>
2017-05-17 11:26:53 +1000

    proptypes

--- src/MultiChoice/Multichoice.test.js
+++ src/MultiChoice/Multichoice.test.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import MultiChoice from './MultiChoice';
 
 it('renders without crashing', () => {
+  const sample_data = [
+    question: 'What is the meaning of life?',
+    correct_answer: '42',
+    possible_answers: ['Love', 'Money', 'JavaScript', '42']
+  ]
   const div = document.createElement('div');
   ReactDOM.render(<MultiChoice />, div);
 });
